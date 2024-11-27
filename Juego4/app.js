e3b08b107119672e5dee690e2bfdd41159b341a4
Jaime Eduardo Carvajal Amado <jamado@novarihealth.com>
2024-11-27 17:12:21 -0500

    return to code

--- Juego4/app.js
+++ Juego4/app.js
@@ -1,21 +1,6 @@
 const gameBoard = document.querySelector("#gameboard");
 
-//const startCells = ["","","","","","","","",""];
-
-const startCells = ["155","5652","654","45544","5","54545","7","844","9"];
-
-startCells.push("10")
-
-/* startCells.forEach( (_cell, index) => {
-    console.log(_cell)
-    console.log(index)
-}) */
-
-const numeros = [10,2,3,4,5,6,7,8,9,10]
-
-/* for (let index = 0; index < numeros.length; index++) {
-    console.log(numeros[index] * 2);  
-} */
+const startCells = ["","","","","","","","",""];
 
 numeros.forEach((numero, index) => {
     console.log(numero * 2)
